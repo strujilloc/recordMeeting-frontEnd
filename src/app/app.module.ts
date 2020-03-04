7f50a8c36befda97ff7d50e33534315c0d4ab0fc
strujilloc <3881022596@edu.k.u-tokyo.ac.jp>
2020-03-04 14:40:04 +0900

    add speech recognition

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,15 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { RouteReuseStrategy } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { RouteReuseStrategy } from "@angular/router";
 
-import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
-import { SplashScreen } from '@ionic-native/splash-screen/ngx';
-import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { IonicModule, IonicRouteStrategy } from "@ionic/angular";
+import { SplashScreen } from "@ionic-native/splash-screen/ngx";
+import { StatusBar } from "@ionic-native/status-bar/ngx";
 
-import { AppComponent } from './app.component';
-import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from "./app.component";
+import { AppRoutingModule } from "./app-routing.module";
+
+import { SpeechRecognition } from "@ionic-native/speech-recognition/ngx";
 
 @NgModule({
   declarations: [AppComponent],
@@ -16,6 +18,7 @@ import { AppRoutingModule } from './app-routing.module';
   providers: [
     StatusBar,
     SplashScreen,
+    SpeechRecognition,
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
   ],
   bootstrap: [AppComponent]
--- src/app/record/record.page.ts
+++ src/app/record/record.page.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from "@angular/core";
 import { Message } from "../message.model";
+import { Platform } from "@ionic/angular";
+import { SpeechRecognition } from "@ionic-native/speech-recognition/ngx";
 
 @Component({
   selector: "app-record",
@@ -14,12 +16,43 @@ export class RecordPage implements OnInit {
     userId: ""
   };
 
-  constructor() {}
+  matches: String[];
+  isRecording = false;
+
+  constructor(
+    private plt: Platform,
+    private speechRecognition: SpeechRecognition
+  ) {}
 
   ngOnInit() {}
 
   sendData() {
     this.data = { meetingId: this.inputMeeting, userId: this.inputUser };
     console.log(this.data);
+  }
+
+  getPermissions() {
+    this.speechRecognition.hasPermission().then((hasPermission: boolean) => {
+      if (!hasPermission) {
+        this.speechRecognition.requestPermission();
+      }
+    });
+    this.isRecording = true;
+  }
+  startRecording() {
+    let options = {
+      language: "en-US"
+    };
+    this.speechRecognition.startListening(options).subscribe(matches => {
+      this.matches = matches;
+    });
+  }
+  stopRecording() {
+    this.speechRecognition.stopListening().then(() => {
+      this.isRecording = false;
+    });
+  }
+  isIos() {
+    return this.plt.is("ios");
   }
 }
